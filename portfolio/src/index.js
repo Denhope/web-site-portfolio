6ff243539503c4c2f49e72b037a913bdfe3a5636
Denis_Koval <den_hope@mail.ru>
2022-01-18 02:32:02 +0300

    feat: add activeClick and changeImage functions

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,5 +1,9 @@
 import { burger } from "./js/_hamburger";
 import { score } from "./js/_score";
+import { portfolio } from "./js/_portfolio";
+import{ test} from "./js/_test";
 burger();
 score();
+portfolio();
+// test();
 
--- portfolio/src/js/_portfolio.js
+++ portfolio/src/js/_portfolio.js
@@ -1,14 +1,56 @@
 export function portfolio() {
+ 
   const portfolioBtn = document.querySelector('.portfolio__button');
-const portfolioImages = document.querySelectorAll('.portfolio__item');
+  const portfolioBtns = document.querySelectorAll('.portfolio__button');
+  const portfolioBtnsContanier = document.querySelector('.portfolio__buttons-container');
+  const portfolioImages = document.querySelectorAll('.portfolio__item');
 
-portfolioImages.forEach((img, index) => img.src = `./src/assets/portfolio/winter/${index + 1}.jpg`);
 
-const portfolioBtns = document.querySelector('.portfolio__buttons-container');
+  document.addEventListener('click', evt => {
 
-function changeImage(event) {
-  if(event.target.classList.contains('portfolio-btn')) {
-    // здесь код функции, меняющей src изображений
+    
+    if(evt.target.classList.contains('portfolio__button')) {
+      let activeBtn = evt.target.dataset['season'];
+      console.log(`нажатие-${activeBtn}`);      
+      portfolioBtns.forEach((item) => {
+        item.classList.remove('button_active');
+          if (!item.classList.contains(activeBtn) ) {
+          evt.target.classList.add('button_active');
+
+          
+          portfolioImages.forEach((img, index) => {
+            img.src = `.src/assets/img/${activeBtn}/${index+ 1}.jpg`;
+            console.log('функция смена изображений');
+          })   
+        }
+     });    
+    }                 
+  })  
+
+  function prelodImages() {
+    const seasons = ['summer', 'winter', 'spring', 'autum'];
+    seasons.forEach((el) => {
+      for (let i =1; i <= 6; i++) {
+        const img = new Image();
+        img.src = `.src/assets/portfolio/${el}/${i}.jpg`;
+        console.log('prelodfunction');
+      }
+    })
   }
+  // prelodImages(); 
+
+
+
+  // function changeImage() {   
+  // portfolioBtn.forEach((item) => {
+  //   portfolioImages.forEach((img, index) => {
+  //     img.src = `.src/assets/portfolio/${item.dataset.season}/${index+ 1}.jpg`;
+  //     console.log('функция');
+  //   })          
+  // })
+// }
 }
-}
+
+
+   
+ 
