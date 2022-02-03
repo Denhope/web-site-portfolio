bf662869fe820b39550bae7595b5d104b5b10a2d
Denis_Koval <den_hope@mail.ru>
2022-02-04 00:52:26 +0300

    feat: add videoprogress and setvolume functions

--- portfolio/src/js/_video.js
+++ portfolio/src/js/_video.js
@@ -1,19 +1,17 @@
 export function playVideo() {
   // controls
   const player = document.querySelector(".player");
-
   const playerVideo = player.querySelector(".player__video");
-
   const playerProgress = player.querySelector(".player__progress");
   const playerVolume = player.querySelector(".player__volume");
-
   const playerPlayButton = player.querySelector(".player__play");
   const playerMuteButton = player.querySelector(".player__mute");
   const playerScreenPlayButton = player.querySelector(".player__screen-button");
   const playerFullscreenButton = player.querySelector(".player__fullscreen");
 
   //listener
-  // play\pause listeners
+
+  // play/pause listeners
   playerPlayButton.addEventListener("click", () => {
     switchPlayback();
   });
@@ -35,14 +33,24 @@ export function playVideo() {
     switchMute();
   });
 
-  playerVolume.addEventListener("click", function () {
-    const level = Number(playerVolume.value);
-    if (level === 0) {
-      mute();
-    } else {
-      unmute();
-      setVolumeLevel(level);
-    }
+  playerVolume.addEventListener("input", function () {
+    setVolumeLevel();
+  });
+
+  //progress-bar video listener
+  playerProgress.addEventListener("input", function () {
+    setVideoProgress();
+  });
+
+  playerVideo.addEventListener("timeupdate", function () {
+    updateVideoProgress();
+  });
+
+  playerProgress.addEventListener("mouseup", function () {
+    startPlay();
+  });
+  playerProgress.addEventListener("mousedown", function () {
+    stopPlay();
   });
 
   // fullscreen listener
@@ -50,7 +58,7 @@ export function playVideo() {
     switchFullscreen();
   });
 
-  //FUNCTIONS
+  //functions
 
   // start/stop functions
   function switchPlayback() {
@@ -92,12 +100,33 @@ export function playVideo() {
     playerMuteButton.classList.remove("player__mute_on");
   }
 
-  function setVolumeLevel(level) {}
+  function setVolumeLevel() {
+    playerVideo.volume = playerVolume.value / 100;
+    let value = playerVolume.value;
+    let level = playerVolume.value / 100;
+    playerVolume.style.background = `linear-gradient(to right, rgb(189, 174, 130) 0%, rgb(189, 174, 130) ${value}%, rgb(200, 200, 200) ${value}%, rgb(200, 200, 200) 100%)`;
+    if (level === 0) mute();
+    else unmute();
+  }
 
   // video position functions
-  function setVideoPosition(position) {}
 
-  //fullscreen
+  //set video progress
+  function setVideoProgress() {
+    // Calculate the new time
+    let time = playerVideo.duration * (playerProgress.value / 100);
+    // Update the video time
+    playerVideo.currentTime = time;
+  }
+
+  //update video progress
+  function updateVideoProgress() {
+    let value = (100 / playerVideo.duration) * playerVideo.currentTime;
+    playerProgress.value = value;
+    playerProgress.style.background = `linear-gradient(to right, rgb(189, 174, 130) 0%, rgb(189, 174, 130) ${value}%, rgb(200, 200, 200) ${value}%, rgb(200, 200, 200) 100%)`;
+  }
+
+  //fullscreen function
   function switchFullscreen() {
     if (document.fullscreenElement === player) {
       playerFullscreenButton.classList.remove("player__fullscreen__exit");
