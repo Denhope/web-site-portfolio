ad019e15b1fd674b35d88793f02af342ec57aa1b
Denis_Koval <den_hope@mail.ru>
2022-03-07 17:37:18 +0300

    refactor: transfer files in repo

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# denhope-JSFEPRESCHOOL
-Private repository for @denhope
+# Welcome to my project!
+
+This project has been created using **webpack-cli**.
+
+## Build Setup:
+
+```bash
+# Download repository:
+git clone git@github.com:Denhope/image-gallery.git
+
+# Go to the app:
+cd web-site-portfolio
+
+# Install dependencies:
+npm install
+```
+
+```bash
+# Server with hot reload at http://localhost: 1000/
+npm run serv
+```
+
+```bash
+# Output will be at dist/ folder for production mode
+npm run prod
+```
+
+```bash
+# Output will be at dist/ folder for development mode
+npm run dev
+```
+
+## You can see my App: https://denhope-portfolio.netlify.app/
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const webpack = require('webpack');
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = (env, options) => {
+  const isProduction = options.mode === 'production';
+  const isAnalyze = env.analyze;
+  const config = {
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? 'source-map' : 'eval',
+    watch: !isProduction,
+    entry: ['./src/index.js', './src/sass/style.scss'],
+    output: {
+      path: path.join(__dirname, '/dist'),
+      filename: 'script.js',
+      assetModuleFilename: 'assets/public/[hash][ext]',
+    },
+
+    resolve: {
+      extensions: ['.js', '.json', '.mjs'],
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.(png|svg|jpe?g|gif)$/,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.html$/,
+          loader: 'html-loader',
+        },
+      ],
+    },
+    devServer: {
+      open: true,
+      port: 1000,
+    },
+
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: 'index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'style.css',
+      }),
+      new CopyPlugin({
+        patterns: [{ from: './src/assets/img', to: './assets/img' }],
+      }),
+    ],
+  };
+
+  return config;
+};
